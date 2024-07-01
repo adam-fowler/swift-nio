9495db85a1fde7249a6232444d79f0d01f793d88
Adam Fowler <adamfowler71@gmail.com>
2024-07-01 16:03:32 +0100

    HTTP upgrades: store inputClosed for processing when unbuffering

--- Sources/NIOHTTP1/NIOTypedHTTPServerUpgradeHandler.swift
+++ Sources/NIOHTTP1/NIOTypedHTTPServerUpgradeHandler.swift
@@ -374,6 +374,8 @@ public final class NIOTypedHTTPServerUpgradeHandler<UpgradeResult: Sendable>: Ch
     private func unbuffer(context: ChannelHandlerContext) {
         while true {
             switch self.stateMachine.unbuffer() {
+            case .close:
+                context.close(promise: nil)
             case .fireChannelRead(let data):
                 context.fireChannelRead(data)
 
